047b0f52b6e7dd26a0172a1c4ed3b72cc59bfd21
zeyn13 <likazeynep@gmail.com>
2023-01-05 16:48:48 +0100

    vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-app-bar app color="3853D8" dark></v-app-bar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import "@fortawesome/fontawesome-free/css/all.css";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+createApp(App).mount("#app");
